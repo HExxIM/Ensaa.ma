5919a3c927b21d39126716b9216030e9a31b24fb
hassanelq <hassanelqadi2@gmail.com>
2023-09-28 21:42:30 +0100

    Hero styling

--- app/Components/Sections/Home/Hero/Hero.jsx
+++ app/Components/Sections/Home/Hero/Hero.jsx
@@ -1,40 +1,61 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from './Hero.module.css'
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Hero.module.css";
 //svgs
-import moonLogo from '../../../../../public/svg/moonLogo.svg';
-import playIcon from '../../../../../public/svg/playIcon.svg'
-import discordLogo from '../../../../../public/svg/discordLogo.svg'
-import instaLogo from '../../../../../public/svg/instaLogo.svg'
-import XLogo from '../../../../../public/svg/XLogo.svg'
-import facebookLogo from '../../../../../public/svg/facebookLogo.svg'
-
+import moonLogo from "../../../../../public/svg/moonLogo.svg";
+import playIcon from "../../../../../public/svg/playIcon.svg";
+import discordLogo from "../../../../../public/svg/discordLogo.svg";
+import instaLogo from "../../../../../public/svg/instaLogo.svg";
+import XLogo from "../../../../../public/svg/XLogo.svg";
+import facebookLogo from "../../../../../public/svg/facebookLogo.svg";
 
 export default function HeroSection() {
-  return (
-    <div className={styles.Hero_container}>
-        <div className={styles.Hero_left}>
-          <h1 className={styles.slogon}>Together we<br/>can make it<br/>real!</h1>
-          <p>We assure u a better environment at ENSAA.</p>
-          <div>
-            <div>
-              <Link href="/">
-                <Image src={playIcon} alt="play" width="26" height="26"></Image>
-                <p>watch a video</p>
-              </Link>
-              <p>25th promotion</p>
-            </div>
-            <div>
-              <Image src={discordLogo} width="37" height="42"></Image>
-              <Image src={instaLogo} width="42" height="42"></Image>
-              <Image src={XLogo} width="49" height="42"></Image>
-              <Image src={facebookLogo} width="42" height="42"></Image>
-            </div>
-          </div>
-        </div>
-        <div>
-          <Image src={moonLogo} alt='moonlogo' height="615" width="750" className={styles.moonLogo} ></Image>
-        </div>
-    </div>
-  )
+	return (
+		<div className={styles.Hero_container}>
+			<div className={styles.Hero_left}>
+				<h1 className={styles.slogon}>
+					Together we
+					<br />
+					can make it
+					<br />
+					real!
+				</h1>
+				<p className={styles.description}>
+					We assure u a better environment at ENSAA.
+				</p>
+				<div>
+					<div>
+						<Link href="/" className={styles.button}>
+							<Image src={playIcon} alt="play"></Image>
+							<p>watch a video</p>
+						</Link>
+						<p>25th promotion</p>
+					</div>
+					<div className={styles.social_media}>
+						<Link href="/">
+							<Image src={discordLogo} height="30" width="30"></Image>
+						</Link>
+						<Link href="/">
+							<Image src={instaLogo} height="30" width="30"></Image>
+						</Link>
+						<Link href="/">
+							<Image src={XLogo} height="30" width="30"></Image>
+						</Link>
+						<Link href="/">
+							<Image src={facebookLogo} height="30" width="30"></Image>
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div>
+				<Image
+					src={moonLogo}
+					alt="moonlogo"
+					height="615"
+					width="750"
+					className={styles.moonLogo}
+				></Image>
+			</div>
+		</div>
+	);
 }
