9284b5d3d0b0c27aee4c4d2f0050b34b95f408a3
ibtissammassa <bettysama16@gmail.com>
2023-09-28 19:52:28 +0100

    import svg

--- app/Components/Sections/Home/Hero/Hero.jsx
+++ app/Components/Sections/Home/Hero/Hero.jsx
@@ -1,6 +1,8 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 //svgs
-import moonLogo from '../../../../public/Assets/moonLogo.svg';
-import playIcon from ''
+import playIcon from '../../../../../public/svg/playIcon.svg'
 
 export default function HeroSection() {
   return (
@@ -9,7 +11,9 @@ export default function HeroSection() {
           <h1>Together we can make it real!</h1>
           <p>We assure u a better environment at ENSAA.</p>
           <div>
-            <Link href="/">{}</Link>
+            <Link href="/">
+              <Image src={playIcon} alt="me" width="64" height="64"></Image>
+            </Link>
           </div>
         </div>
         <div></div>
