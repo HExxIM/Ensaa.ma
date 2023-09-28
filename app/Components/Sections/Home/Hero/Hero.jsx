b884a1a9510ede8c7d182c17a4832b1d9fb97434
ibtissammassa <bettysama16@gmail.com>
2023-09-28 19:32:34 +0100

    Hero left

--- app/Components/Sections/Home/Hero/Hero.jsx
+++ app/Components/Sections/Home/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 //svgs
 import moonLogo from '../../../../public/Assets/moonLogo.svg';
+import playIcon from ''
 
 export default function HeroSection() {
   return (
     <div>
-        <div></div>
+        <div>
+          <h1>Together we can make it real!</h1>
+          <p>We assure u a better environment at ENSAA.</p>
+          <div>
+            <Link href="/">{}</Link>
+          </div>
+        </div>
         <div></div>
     </div>
   )
