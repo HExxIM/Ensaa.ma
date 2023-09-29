818ef062f055896ee00a61fa0a072cadc80170c4
hassanelq <hassanelqadi2@gmail.com>
2023-09-29 21:09:14 +0100

    Footer 80%

--- app/Components/Sections/footer/Footer.jsx
+++ app/Components/Sections/footer/Footer.jsx
@@ -5,6 +5,8 @@ import styles from "./Footer.module.css";
 //svgs
 import ADELogo from "../../../../public/svg/ADELogo.svg";
 import Rectangle from "../../../../public/svg/Rectangle.svg";
+import contactUsTitle from "../../../../public/svg/contactUsTitle.svg";
+import shapeBlurBottom from "../../../../public/svg/shapeBlurBottom.svg";
 
 //images
 import eatrh from "../../../../public/images/earth.webp";
@@ -16,44 +18,67 @@ import Button from "../../UI/Button";
 export default function Footer() {
   return (
     <div className={styles.Footer_container}>
-      {/* <Image src={eatrh} alt="Eatrh" className={styles.eatrh}></Image> */}
+      <Image src={eatrh} alt="Eatrh" className={styles.eatrh}></Image>
+      <Image
+        src={shapeBlurBottom}
+        alt="shapeBlurBottom"
+        className={styles.shapeBlurBottom}
+      ></Image>
+
       <div className={styles.ContactUs}>
-        <h1>Contact us</h1>
-        <div className={styles.form}>
-          <label htmlFor="name">Your Name</label>
-          <textarea name="Your Name" id="name" cols="30" rows="10"></textarea>
-          <label htmlFor="email">Your Email</label>
-          <textarea name="Your Email" id="email" cols="30" rows="10"></textarea>
-          <label htmlFor="text">Your Text</label>
-          <textarea name="Your Text" id="text" cols="30" rows="10"></textarea>
+        <Image
+          src={contactUsTitle}
+          alt="contact Us Title"
+          className={styles.contactUsTitle}
+        ></Image>
+        <form>
+          <div>
+            <label htmlFor="name">Your Name</label>
+            <textarea name="Your Name" id="name" cols="40" rows="1"></textarea>
+          </div>
+          <div>
+            <label htmlFor="email">Your Email</label>
+            <textarea
+              name="Your Email"
+              id="email"
+              cols="40"
+              rows="1"
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="text">Your Text</label>
+            <textarea name="Your Text" id="text" cols="40" rows="5"></textarea>
+          </div>
           <Button text="send" link="/" />
-        </div>
+        </form>
       </div>
-      <div className={styles.logo_socials_links}>
-        <div className={styles.logo_socials}>
-          <Image
-            src={ADELogo}
-            alt="ADE logo"
-            className={styles.ADELogo}
-          ></Image>
-          <Image
-            src={Rectangle}
-            alt="Rectangle"
-            className={styles.Rectangle}
-          ></Image>
-          <SocialMediaList iconsSize="25" iconsGap="1rem" />
+      <div className={styles.bottom_elements}>
+        <div className={styles.logo_socials_links}>
+          <div className={styles.logo_socials}>
+            <Image
+              src={ADELogo}
+              alt="ADE logo"
+              className={styles.ADELogo}
+            ></Image>
+            <Image
+              src={Rectangle}
+              alt="Rectangle"
+              className={styles.Rectangle}
+            ></Image>
+            <SocialMediaList iconsSize="25" iconsGap="1rem" />
+          </div>
+          <div className={styles.links}>
+            <Link href="/" target="_blank">
+              Terms of service
+            </Link>
+            <Link href="/">Contact</Link>
+            <Link href="/">Report</Link>
+          </div>
         </div>
-        <div className={styles.links}>
-          <Link href="/" target="_blank">
-            Terms of service
-          </Link>
-          <Link href="/">Contact</Link>
-          <Link href="/">Report</Link>
-        </div>
+        <p className={styles.copyrights}>
+          Copyright © ADE ENSAA | Designed by ZEUS
+        </p>
       </div>
-      <p className={styles.copyrights}>
-        Copyright © ADE ENSAA | Designed by ZEUS
-      </p>
     </div>
   );
 }
