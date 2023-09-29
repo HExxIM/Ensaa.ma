525341baa1d2ef71c4607fed5a56407cb75e9b19
hassanelq <hassanelqadi2@gmail.com>
2023-09-29 04:38:35 +0100

    SocialMediaLinks

--- app/Components/Sections/Home/Hero/Hero.jsx
+++ app/Components/Sections/Home/Hero/Hero.jsx
@@ -6,51 +6,74 @@ import moonLogo from "../../../../../public/svg/moonLogo.svg";
 import playIcon from "../../../../../public/svg/playIcon.svg";
 import discordLogo from "../../../../../public/svg/discordLogo.svg";
 import instaLogo from "../../../../../public/svg/instaLogo.svg";
-import XLogo from "../../../../../public/svg/XLogo.svg";
+import linkedinLogo from "../../../../../public/svg/linkedinLogo.svg";
 import facebookLogo from "../../../../../public/svg/facebookLogo.svg";
+import XLogo from "../../../../../public/svg/XLogo.svg";
+import shapeBlurBottom from "../../../../../public/svg/shapeBlurBottom.svg";
+import shapeBlurTop from "../../../../../public/svg/shapeBlurTop.svg";
 
 export default function HeroSection() {
-	return (
-		<div className={styles.Hero_container}>
-			<div className={styles.Hero_left}>
-				<div className={styles.Hero_texts}>
-					<h1>Together we</h1>
-					<h1>can make it</h1>
-					<h1>real!</h1>
-					<p className={styles.description}>
-						We assure you a better environment at ENSAA.
-					</p>
-					<div className={styles.button_promo}>
-						<Link href="/" className={styles.button}>
-							<Image src={playIcon} alt="play" className={styles.play}></Image>
-							<p>watch a video</p>
-						</Link>
-						<p>25th promotion</p>
-					</div>
-				</div>
-				<div className={styles.social_media}>
-					<Link href="/">
-						<Image src={discordLogo} height="30" width="30"></Image>
-					</Link>
-					<Link href="/">
-						<Image src={instaLogo} height="30" width="30"></Image>
-					</Link>
-					<Link href="/">
-						<Image src={XLogo} height="30" width="30"></Image>
-					</Link>
-					<Link href="/">
-						<Image src={facebookLogo} height="30" width="30"></Image>
-					</Link>
-				</div>
-			</div>
+  const socialMedia = [
+    {
+      name: "instagram",
+      link: "https://www.instagram.com/ade.ensaa/",
+      logo: instaLogo,
+    },
+    {
+      name: "facebook",
+      link: "https://www.facebook.com/adeensaa/",
+      logo: facebookLogo,
+    },
+    {
+      name: "linkedin",
+      link: "https://www.linkedin.com/company/ade-ensa-agadir",
+      logo: linkedinLogo,
+    },
+    {
+      name: "X",
+      link: "#",
+      logo: XLogo,
+    },
+    {
+      name: "discord",
+      link: "https://discord.gg/3FyqYt7",
+      logo: discordLogo,
+    },
+  ];
 
-			<div className={styles.Hero_right}>
-				<Image
-					src={moonLogo}
-					alt="moonlogo"
-					className={styles.moonLogo}
-				></Image>
-			</div>
-		</div>
-	);
+  const socialMediaList = socialMedia.map((media) => (
+    <Link href={media.link} key={media.name} target="_blank">
+      <Image src={media.logo} height="30" width="30" alt={media.name} />
+    </Link>
+  ));
+
+  return (
+    <div className={styles.Hero_container}>
+      <div className={styles.Hero_left}>
+        <div className={styles.Hero_texts}>
+          <h1>Together we</h1>
+          <h1>can make it</h1>
+          <h1>real!</h1>
+          <p className={styles.description}>
+            We assure you a better environment at ENSAA.
+          </p>
+          <div className={styles.button_promo}>
+            <Link href="/" className={styles.button}>
+              <Image src={playIcon} alt="play" className={styles.play}></Image>
+              <p>watch a video</p>
+            </Link>
+            <p>25th promotion</p>
+          </div>
+        </div>
+        <div className={styles.social_media}>{socialMediaList}</div>
+      </div>
+      <div className={styles.Hero_right}>
+        <Image
+          src={moonLogo}
+          alt="moonlogo"
+          className={styles.moonLogo}
+        ></Image>
+      </div>
+    </div>
+  );
 }
