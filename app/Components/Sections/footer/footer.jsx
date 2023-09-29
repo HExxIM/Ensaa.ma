7a4398b8f191017ef62f7188e6c4a2e7c1fdaf6a
hassanelq <hassanelqadi2@gmail.com>
2023-09-29 18:30:08 +0100

    footer

--- app/Components/Sections/footer/footer.jsx
+++ app/Components/Sections/footer/footer.jsx
@@ -4,6 +4,9 @@ import styles from "./Footer.module.css";
 //svgs
 import ADELogo from "../../../../public/svg/ADELogo.svg";
 
+//images
+import eatrh from "../../../../public/images/earth.webp";
+
 //components
 import SocialMediaList from "../../UI/SocialMediaList";
 import Button from "../../UI/Button";
@@ -12,6 +15,7 @@ export default function Footer() {
   return (
     <div className={styles.Footer_container}>
       <Image src={ADELogo} alt="ADE logo" className={styles.ADELogo}></Image>
+      <Image src={eatrh} alt="Eatrh" className={styles.eatrh}></Image>
     </div>
   );
 }
