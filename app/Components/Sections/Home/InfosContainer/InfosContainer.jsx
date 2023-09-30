687be216d6b0d15be6970e0ac6d60d71fc7a6403
hassanelq <hassanelqadi2@gmail.com>
2023-09-30 12:28:05 +0100

    Info merged

--- app/Components/Sections/Home/InfosContainer/InfosContainer.jsx
+++ app/Components/Sections/Home/InfosContainer/InfosContainer.jsx
@@ -1,20 +1,14 @@
-<<<<<<< HEAD:app/Components/UI/InfosContainer/InfosContainer.jsx
 import Image from "next/image";
 import styles from "./infosContainer.module.css";
 //svg
 import shapeBlur from "../../../../public/images/shapeBlur.webp";
 //components
-import TitleBox from '../../../UI/TitleBox';
-=======
->>>>>>> c44abb8f4f72f4b3c1a7306d26c13673642625e0:app/Components/Sections/Home/InfosContainer/InfosContainer.jsx
+import TitleBox from "../../../UI/TitleBox";
 
 export default function InfosContainer({ text, logo, title }) {
   return (
     <div className={styles.container}>
-<<<<<<< HEAD:app/Components/UI/InfosContainer/InfosContainer.jsx
-<TitleBox title={title}/>
-=======
->>>>>>> c44abb8f4f72f4b3c1a7306d26c13673642625e0:app/Components/Sections/Home/InfosContainer/InfosContainer.jsx
+      <TitleBox title={title} />
       <div className={styles.mainInfos}>
         <p className={styles.text}>{text}</p>
         <Image className={styles.logo} src={logo}></Image>
