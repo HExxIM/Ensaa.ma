353ca4368b755be798c255ef22d39aa935f6f886
ibtissammassa <bettysama16@gmail.com>
2023-09-30 15:58:10 +0100

    form validating

--- app/Components/UI/FooterForm/FooterForm.jsx
+++ app/Components/UI/FooterForm/FooterForm.jsx
@@ -1,28 +1,95 @@
-import Styles from './FooterForm.module.css'
+'use client'
+import Styles from './FooterForm.module.css';
+import { useState,useEffect,useRef } from 'react';
 //components
 import Button from "../../UI/Button";
 
 export default function FooterForm() {
+    const formRef = useRef(null);
+    const [inputFields, setInputFields] = useState({
+        name: "",
+        email: "",
+        text: null
+    });
+    const [errors, setErrors] = useState({});
+    const [submitting, setSubmitting] = useState(false);
+
+    const validateValues = (inputValues) => {
+        let errors = {};
+        if(inputValues.name){
+            if (inputValues.name.length < 30) {
+              errors.name = "Name too long!";
+            }
+        }else{
+            errors.name = "Name is Required!";
+        }
+        if (!inputValues.email) {
+          errors.email = "Invalid email address";
+        }
+        if (!inputValues.text ) {
+          errors.text = "Please enter your message";
+        }
+        return errors;
+    };
+
+    const handleChange = (e) => {
+        setInputFields({ ...inputFields, [e.target.name]: e.target.value });
+    };
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(validateValues(inputFields));
+        setSubmitting(true);
+    };
+
+    const finishSubmit = () => {
+        console.log(inputFields);
+    };
+
+    useEffect(() => {
+        if (Object.keys(errors).length === 0 && submitting) {
+            finishSubmit();
+        }
+    }, [errors]);
+
   return (
-    <form className={Styles.form}>
-        <div>
-            <label htmlFor="name">Your Name</label>
-            <textarea name="Your Name" id="name" cols="40" rows="1"></textarea>
-        </div>
-        <div>
-            <label htmlFor="email">Your Email</label>
-            <textarea
-                name="Your Email"
-                id="email"
-                cols="40"
-                rows="1"
-            ></textarea>
-        </div>
-        <div>
-            <label htmlFor="text">Your Text</label>
-            <textarea name="Your Text" id="text" cols="40" rows="5"></textarea>
-        </div>
-        <Button text="send" link="/" />
-  </form>
+    <>
+        {Object.keys(errors).length === 0 && submitting ? (
+            <span className="success">Successfully submitted ✓</span>
+        ) : null}
+        <form className={Styles.form} onSubmit={handleSubmit} ref={formRef}>
+            <div>
+                <label htmlFor="name">Your Name</label>
+                <input value={inputFields.name} onChange={handleChange} type='text' name="Your Name" id="name" cols="40" rows="1"></input>
+            </div>
+            <div>
+                <label htmlFor="email">Your Email</label>
+                <input
+                    value={inputFields.email}
+                    onChange={handleChange}
+                    type='email'
+                    name="Your Email"
+                    id="email"
+                    cols="40"
+                    rows="1"
+                ></input>
+            </div>
+            <div>
+                <label htmlFor="text">Your Text</label>
+                <textarea value={inputFields.text} onChange={handleChange} name="Your Text" id="text" cols="40" rows="5"></textarea>
+            </div>
+            <Button 
+                text="send" 
+                link="/" 
+                onclick={
+                    (e) => {
+                            e.preventDefault();
+                            if (formRef.current) {
+                                formRef.current.submit();
+                            }}
+                        }
+            />
+    </form>
+  </>
   )
 }
