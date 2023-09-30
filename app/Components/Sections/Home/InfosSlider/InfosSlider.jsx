beb3e8b4f1a5126ae70b2fc97e629bced1dad515
ibtissammassa <bettysama16@gmail.com>
2023-09-30 12:28:25 +0100

    infosSlider

--- app/Components/Sections/Home/InfosSlider/InfosSlider.jsx
+++ app/Components/Sections/Home/InfosSlider/InfosSlider.jsx
@@ -1,9 +1,13 @@
 
 
-export default function InfosSlider() {
+export default function InfosSlider({title,images,boxHeight}) {
+    const imagesSlider = images.map()
   return (
     <div>
-        
+        <TitleBox title={title}/>
+        <div>
+            
+        </div>
     </div>
   )
 }
