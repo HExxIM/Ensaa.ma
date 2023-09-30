4078219cc748d008d57786e0b63cf02f02476651
hassanelq <hassanelqadi2@gmail.com>
2023-09-30 17:11:27 +0100

    Api sned mail

--- app/Components/UI/FooterForm/FooterForm.jsx
+++ app/Components/UI/FooterForm/FooterForm.jsx
@@ -1,85 +1,121 @@
-'use client'
-import Styles from './FooterForm.module.css';
-import { useState,useEffect,useRef } from 'react';
+"use client";
+
+import { useState, useEffect } from "react";
+import Styles from "./FooterForm.module.css";
 
 export default function FooterForm() {
-    const [inputFields, setInputFields] = useState({
-        name: "",
-        email: "",
-        text: null
-    });
-    const [errors, setErrors] = useState({});
-    const [submitting, setSubmitting] = useState(false);
+  const [inputFields, setInputFields] = useState({
+    name: "",
+    email: "",
+    text: "",
+  });
+  const { name, email, text } = inputFields;
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
 
-    const validateValues = (inputValues) => {
-        let errors = {};
-        if(inputValues.name){
-            if (inputValues.name.length > 30) {
-              errors.name = "Name too long!";
-            }
-        }else{
-            errors.name = "Name is Required!";
-        }
-        if (!inputValues.email) {
-          errors.email = "Invalid email address";
-        }
-        if (!inputValues.text ) {
-          errors.text = "Please enter your message";
-        }
-        return errors;
-    };
+  const validateValues = (inputValues) => {
+    let errors = {};
 
-    const handleChange = (e) => {
-        setInputFields({ ...inputFields, [e.target.name]: e.target.value });
-    };
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setErrors(validateValues(inputFields));
-        setSubmitting(true);
-    };
+    if (!inputValues.name) {
+      errors.name = "Name is Required!";
+    } else if (inputValues.name.length > 30) {
+      errors.name = "Name too long!";
+    }
 
-    const finishSubmit = () => {
-        console.log(inputFields);
-    };
+    if (!inputValues.email) {
+      errors.email = "Email is Required!";
+    } else if (!/\S+@\S+\.\S+/.test(inputValues.email)) {
+      errors.email = "Invalid email address";
+    }
 
-    useEffect(() => {
-        if (Object.keys(errors).length === 0 && submitting) {
-            finishSubmit();
-        }
-    }, [errors]);
+    if (!inputValues.text) {
+      errors.text = "Please enter your message";
+    }
+
+    return errors;
+  };
+
+  const handleChange = (e) => {
+    setInputFields({ ...inputFields, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validateValues(inputFields));
+    setSubmitting(true);
+  };
+
+  const finishSubmit = async () => {
+    try {
+      const response = await fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputFields),
+      });
+
+      if (response.status === 200) {
+        console.log("Message sent successfully");
+      } else {
+        console.error("Error sending message");
+      }
+    } catch (error) {
+      console.error("There was an error sending the message", error);
+    }
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && submitting) {
+      finishSubmit();
+    }
+  }, [errors]);
 
   return (
-    <>
-        <form className={Styles.form} onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Your Name</label>
-                <input value={inputFields.name} onChange={handleChange} type='text' name="name" id="name" cols="40" rows="1"></input>
-                {errors.name && <span className={Styles.error}>{errors.name}</span>}
-            </div>
-            <div>
-                <label htmlFor="email">Your Email</label>
-                <input
-                    value={inputFields.email}
-                    onChange={handleChange}
-                    type='email'
-                    name="email"
-                    id="email"
-                    cols="40"
-                    rows="1"
-                ></input>
-                {errors.email && <span className={Styles.error}>{errors.email}</span>}
-            </div>
-            <div>
-                <label htmlFor="text">Your Text</label>
-                <textarea value={inputFields.text} onChange={handleChange} name="text" id="text" cols="40" rows="5"></textarea>
-                {errors.text && <span className={Styles.error}>{errors.text}</span>}
-            </div>
-            <button className={Styles.button} type="submit">send</button>
+    <form className={Styles.form} onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={name}
+          onChange={handleChange}
+          type="text"
+          name="name"
+          id="name"
+        />
+        {errors.name && <span className={Styles.error}>{errors.name}</span>}
+      </div>
+
+      <div>
+        <label htmlFor="email">Your Email</label>
+        <input
+          value={email}
+          onChange={handleChange}
+          type="email"
+          name="email"
+          id="email"
+        />
+        {errors.email && <span className={Styles.error}>{errors.email}</span>}
+      </div>
+
+      <div>
+        <label htmlFor="text">Your Text</label>
+        <textarea
+          value={text}
+          onChange={handleChange}
+          name="text"
+          id="text"
+          cols="40"
+          rows="5"
+        />
+        {errors.text && <span className={Styles.error}>{errors.text}</span>}
+      </div>
+
+      <button className={Styles.button} type="submit">
+        send
+      </button>
+      {Object.keys(errors).length === 0 && submitting && (
+        <span className={Styles.success}>Successfully submitted ✓</span>
+      )}
     </form>
-    {Object.keys(errors).length === 0 && submitting ? (
-            <span className="success">Successfully submitted ✓</span>
-        ) : null}
-  </>
-  )
+  );
 }
