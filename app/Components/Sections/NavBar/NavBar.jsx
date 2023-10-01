f2ce0f124d16e66e2ab531a038b25c59eb914b8a
ibtissammassa <bettysama16@gmail.com>
2023-10-01 19:14:38 +0100

    navBar update

--- app/Components/Sections/NavBar/NavBar.jsx
+++ app/Components/Sections/NavBar/NavBar.jsx
@@ -29,6 +29,10 @@ export default function NavBar() {
       title: "Press",
       path: "/Press",
     },
+    {
+      title: "Team",
+      path: "/Team",
+    },
   ];
 
   const ElementActiveStyles = (path) => {
