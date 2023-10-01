87e10420e00ba1d86b13ead193251e249423770a
ibtissammassa <bettysama16@gmail.com>
2023-10-01 23:17:13 +0100

    edits on emptystate

--- app/Components/Sections/EmptyState/EmptyState.jsx
+++ app/Components/Sections/EmptyState/EmptyState.jsx
@@ -14,7 +14,7 @@ export default function EmptyState() {
             color: "white"
         },
         p:{
-            fontSize: "2.5rem",
+            fontSize: "2.2rem",
             fontWeight: "200",
         }
     }
