d727d549f7a479c6eaf43afe6a518ce2858dc4a2
hassanelq <hassanelqadi2@gmail.com>
2023-10-05 21:53:56 +0100

    Hero texts

--- app/Components/Sections/Home/Hero/Hero.jsx
+++ app/Components/Sections/Home/Hero/Hero.jsx
@@ -17,21 +17,24 @@ export default function HeroSection() {
       <Image src={shapeBlur} className={styles.blurTop}></Image>
 
       <div className={styles.Hero_left}>
-        <div className={styles.Hero_texts}>
-          <h1>Together we</h1>
-          <h1>can make it</h1>
-          <h1>real!</h1>
-          <p className={styles.description}>
-            We assure you a better environment at ENSAA.
-          </p>
-          <div className={styles.button_promo}>
-            <Button text="watch a video" link="/" icon={playIcon} />
-            <p>25th promotion</p>
-          </div>
+        <h1>
+          Together we
+          <br />
+          can make it
+          <br />
+          real!
+        </h1>
+        <p className={styles.description}>
+          We assure you a better environment at ENSAA.
+        </p>
+        <div className={styles.button_promo}>
+          <Button text="watch a video" link="/" icon={playIcon} />
+          <p>25th promotion</p>
         </div>
+      </div>
+      <div className={styles.socialMedia}>
         <SocialMediaList iconsSize="30" iconsGap="1.2rem" brightness="1" />
       </div>
-
       <div className={styles.Hero_right}>
         <Image
           src={moonLogo}
