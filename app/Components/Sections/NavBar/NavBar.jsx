179d9db99826d558745b307a81885919f8597e2a
hassanelq <hassanelqadi2@gmail.com>
2023-10-08 15:03:46 +0100

    Nav

--- app/Components/Sections/NavBar/NavBar.jsx
+++ app/Components/Sections/NavBar/NavBar.jsx
@@ -2,40 +2,14 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import "./NavBar.css";
+
 //svgs
 import dreamersLogo from "../../../../public/svg/AdeAssets/dreamersLogo.svg";
+import navMobile from "../../../../public/svg/icons/navMobile.svg";
 
 //components
 import Button from "../../UI/Button";
-
-const navStyles = {
-  NavContainer: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "11%",
-    width: "100%",
-    position: "fixed",
-    zIndex: 10,
-    background:
-      "linear-gradient(180deg, rgba(63, 215, 78, 0.03) 0%, rgba(63, 215, 78, 0.03) 75.48%, rgba(63, 215, 78, 0) 100%)",
-    backdropFilter: "blur(5px)",
-    paddingLeft: "4%",
-    paddingRight: "5%",
-  },
-  logo: {
-    transform: "scale(0.8)",
-  },
-  NavList: {
-    display: "flex",
-    justifyContent: "space-between",
-    width: "25vw",
-    fontFamily: "var(--font-primary)",
-    fontSize: "1rem",
-    textTransform: "uppercase",
-    fontWeight: 500,
-  },
-};
 
 export default function NavBar() {
   const pathname = usePathname();
@@ -85,11 +59,11 @@ export default function NavBar() {
   ));
 
   return (
-    <div style={navStyles.NavContainer}>
-      <Link style={navStyles.logo} href="/">
+    <div className="NavContainer">
+      <Link className="logo" href="/">
         <Image src={dreamersLogo}></Image>
       </Link>
-      <div style={navStyles.NavList}>{NavList}</div>
+      <div className="NavList">{NavList}</div>
       <Button text="Contact us" link="/Contact" />
     </div>
   );
