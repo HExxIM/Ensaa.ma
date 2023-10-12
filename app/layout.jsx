17b38790572def11d3a3295c40c0757fe318c2fe
ibtissammassa <bettysama16@gmail.com>
2023-10-12 17:57:10 +0100

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from 'next/head';
 
 //components
 import NavBar from "./Components/Sections/NavBar/NavBar";
@@ -15,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href='/favicon.png' />
+      </Head>
       <body className={inter.className}>
         <NavBar />
         {children}
