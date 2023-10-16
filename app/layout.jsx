330d31a866f18c8e75f26ecb796bdab2fbeefa81
hassanelq <hassanelqadi2@gmail.com>
2023-10-16 16:46:03 +0100

    Fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,9 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 import Head from "next/head";
 
 //components
 import NavBar from "./Components/Sections/NavBar/NavBar";
 import Footer from "./Components/Sections/footer/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ensaa",
@@ -29,7 +26,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
       </Head>
-      <body className={inter.className}>
+      <body>
         <NavBar />
         {children}
         <Footer />
