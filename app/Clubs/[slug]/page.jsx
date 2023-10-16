dc5116f6bbbeaffd73296f38c04bae412122f071
ibtissammassa <bettysama16@gmail.com>
2023-10-17 00:12:05 +0100

    club metadata

--- app/Clubs/[slug]/page.jsx
+++ app/Clubs/[slug]/page.jsx
@@ -1,8 +1,18 @@
 import styles from './page.module.css'
+import Head from 'next/head'
+
+
+export const metadata = {
+  description: "Explore the diverse clubs and organizations at ENSAA.",
+  keywords: "ADE, Dreamers, ENSAA, clubs, organizations, student groups, explore"
+};
 
 export default function page({params}) {
   const slug = params.slug;
+  metadata.title = `ENSAA - Clubs - ${slug}`;
   return (
-    <div className={styles.container}>{slug}</div>
+    <div className={styles.container}>
+      {slug}
+      </div>
   )
 }
