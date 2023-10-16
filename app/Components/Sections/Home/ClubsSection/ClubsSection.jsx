0510974efa69e7995219b1f8d5f7137504e4c10d
hassanelq <hassanelqadi2@gmail.com>
2023-10-16 17:04:01 +0100

    ..

--- app/Components/Sections/Home/ClubsSection/ClubsSection.jsx
+++ app/Components/Sections/Home/ClubsSection/ClubsSection.jsx
@@ -8,31 +8,31 @@ import shapeBlur from "../../../../../public/images/shapeBlur.webp";
 
 //components
 import TitleBox from "../../../UI/TitleBox";
-import ClubsInfosList from "./ClubsInfos.jsx";
+import ClubsInfosList from "./ClubsInfosList.jsx";
 
 export default function ClubsSection() {
-  const carouselRef = useRef(null);
-  const isDown = useRef(false);
-  const startX = useRef(null);
-  const scrollLeft = useRef(null);
+  // const carouselRef = useRef(null);
+  // const isDown = useRef(false);
+  // const startX = useRef(null);
+  // const scrollLeft = useRef(null);
 
-  const handleMouseDown = (e) => {
-    isDown.current = true;
-    startX.current = e.pageX - carouselRef.current.offsetLeft;
-    scrollLeft.current = carouselRef.current.scrollLeft;
-  };
+  // const handleMouseDown = (e) => {
+  //   isDown.current = true;
+  //   startX.current = e.pageX - carouselRef.current.offsetLeft;
+  //   scrollLeft.current = carouselRef.current.scrollLeft;
+  // };
 
-  const handleMouseUp = () => {
-    isDown.current = false;
-  };
+  // const handleMouseUp = () => {
+  //   isDown.current = false;
+  // };
 
-  const handleMouseMove = (e) => {
-    if (!isDown.current) return;
-    e.preventDefault();
-    const x = e.pageX - carouselRef.current.offsetLeft;
-    const walk = x - startX.current;
-    carouselRef.current.scrollLeft = scrollLeft.current - walk;
-  };
+  // const handleMouseMove = (e) => {
+  //   if (!isDown.current) return;
+  //   e.preventDefault();
+  //   const x = e.pageX - carouselRef.current.offsetLeft;
+  //   const walk = x - startX.current;
+  //   carouselRef.current.scrollLeft = scrollLeft.current - walk;
+  // };
   return (
     <div className={styles.container}>
       <div className={styles.titleBox}>
@@ -40,10 +40,10 @@ export default function ClubsSection() {
       </div>
       <div
         className={styles.Slider}
-        ref={carouselRef}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseMove={handleMouseMove}
+        // ref={carouselRef}
+        // onMouseDown={handleMouseDown}
+        // onMouseUp={handleMouseUp}
+        // onMouseMove={handleMouseMove}
       >
         <ClubsInfosList />
       </div>
