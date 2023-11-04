61d89ae5261789e9e067c90d1a457bf81ab9fb4a
hassanelq <hassanelqadi2@gmail.com>
2023-11-04 01:59:05 +0100

    Club card media queries

--- app/Components/UI/ClubCard/ClubCard.jsx
+++ app/Components/UI/ClubCard/ClubCard.jsx
@@ -1,33 +1,19 @@
-"use client";
 import styles from "./ClubCard.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import useViewportWidth from "../../../hooks/useViewportWidth";
 //components
 import SocialMediaList from "../../UI/SocialMediaList";
 //svg
 import link from "../../../../public/svg/icons/link.svg";
-export default function ClubCard({
-  image,
-  name,
-  description,
-  links,
-  index,
-  slug,
-}) {
-  const windowWidth = useViewportWidth();
-
-  const flexdirection =
-    windowWidth <= 800 ? "" : index % 2 === 0 ? "row" : "row-reverse";
-  const socialsDirection = windowWidth <= 800 ? "row" : "column";
+export default function ClubCard({ image, name, description, links, slug }) {
   return (
-    <div style={{ flexDirection: flexdirection }} className={styles.card}>
+    <div className={styles.card}>
       <Link href={"/Clubs/" + slug} className={styles.imgContainer}>
         <Image width={120} src={image} alt={name} />
       </Link>
       <div className={styles.SocialMediaList}>
         <SocialMediaList
-          direction={socialsDirection}
+          direction="column"
           className={styles.SocialMediaList}
           links={links}
           iconsSize="23"
