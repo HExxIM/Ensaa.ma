a87ad25701f747f6ac77ea99ebe5e2e012be1f7a
ibtissammassa <bettysama16@gmail.com>
2023-11-13 20:08:13 +0100

    Add lazy loading to YouTube iframe

--- app/News/[category]/[slug]/page.jsx
+++ app/News/[category]/[slug]/page.jsx
@@ -30,7 +30,16 @@ export default function page({ params }) {
         </>
       ))}
       <div className={styles.iframeContainer}>
-        <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+New.youtubeId} title={New.slug} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe 
+          width="560" 
+          height="315" 
+          src={"https://www.youtube.com/embed/"+New.youtubeId} 
+          title={New.slug} 
+          frameBorder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowFullScreen
+          loading="lazy"
+        ></iframe>
       </div>
     </div>
   );
