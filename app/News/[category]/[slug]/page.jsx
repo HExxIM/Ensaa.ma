cf450b3c7c467b55ecc16e441f95bd357fe65596
ibtissammassa <bettysama16@gmail.com>
2023-11-13 19:34:51 +0100

    Add YouTube video to news page

--- app/News/[category]/[slug]/page.jsx
+++ app/News/[category]/[slug]/page.jsx
@@ -29,6 +29,10 @@ export default function page({ params }) {
           <p>{item.text}</p>
         </>
       ))}
+      <div className={styles.iframeContainer}>
+        <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+New.youtubeId} title={New.slug} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      </div>
     </div>
   );
+
 }
