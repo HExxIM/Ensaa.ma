32b1e271c530de4c3cb065a3a70225a094cc1954
hassanelq <hassanelqadi2@gmail.com>
2023-11-14 22:16:45 +0100

    Update UI styles and team page title

--- app/Developpers/page.jsx
+++ app/Developpers/page.jsx
@@ -6,7 +6,7 @@ import TeamDevsList from "./TeamDevsList";
 export default function page() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>TetraTechs</h1>
+      <h1 className={styles.title}>Dev Team</h1>
       <div className={styles.Team}>
         <TeamDevsList />
       </div>
