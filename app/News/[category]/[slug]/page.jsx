33847002b1cd6903f4e75b791a1117621e578fe4
ibtissammassa <bettysama16@gmail.com>
2023-11-14 23:15:25 +0100

    Add metadata to News page

--- app/News/[category]/[slug]/page.jsx
+++ app/News/[category]/[slug]/page.jsx
@@ -4,8 +4,15 @@ import Image from "next/image";
 import news from "../../../Database/news.js";
 //components
 import SocialMediaList from "../../../Components/UI/SocialMediaList";
+
+export const metadata = {
+  description: "Stay up-to-date with the latest news and updates from ENSAA.",
+  keywords: "ADE, Dreamers, ENSAA, news, updates, articles, announcements",
+};
+
 export default function page({ params }) {
   const slug = params.slug;
+  metadata.title = `ENSAA - News - ${slug}`;
   const New = news.find((item) => item.slug === slug);
   return (
     <div className={styles.container}>
@@ -27,7 +34,7 @@ export default function page({ params }) {
           {item.img && (
             <div className={styles.imgContainer}>
               <Image
-                // placeholder="blur"
+                placeholder="blur"
                 className={styles.img}
                 alt={slug}
                 src={item.img}
