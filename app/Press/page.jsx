f1ea6b7056f3e7b78ea093f90570d6d9b7fd1927
hassanelq <hassanelqadi2@gmail.com>
2023-11-14 23:15:31 +0100

    Refactor CSS styles and update Press page with Ensapress image

--- app/Press/page.jsx
+++ app/Press/page.jsx
@@ -1,14 +1,26 @@
 //components
-import EmptyState from "../Components/Sections/EmptyState/EmptyState";
+import MemberCard from "../Components/UI/MemberCard";
+import styles from "./page.module.css";
+import Ensapress from "./ensaPress.png";
 
 export const metadata = {
   title: "ENSAA - Press",
   description: "Read about ENSAA in the press and media coverage.",
-  keywords: "ADE, Dreamers, ENSAA, press, media, coverage, articles, news"
+  keywords: "ADE, Dreamers, ENSAA, press, media, coverage, articles, news",
 };
 
 export default function page() {
   return (
-    <EmptyState/>
-  )
+    <div className={styles.container}>
+      <MemberCard
+        key="Ensa Press"
+        image={Ensapress}
+        name="Ensa Press"
+        post=""
+        linkedin="https://www.instagram.com/press.ensaa/"
+        alt="Ensaa Press"
+        description="A community of individuals dedicated to documenting and sharing noteworthy events within our school."
+      />
+    </div>
+  );
 }
