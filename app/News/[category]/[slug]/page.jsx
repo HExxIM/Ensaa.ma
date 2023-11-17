75067f6961e0ecb19fbece773b67950067a338e4
ibtissammassa <bettysama16@gmail.com>
2023-11-17 15:02:54 +0100

    Refactor YouTube iframe and responsive design in News page

--- app/News/[category]/[slug]/page.jsx
+++ app/News/[category]/[slug]/page.jsx
@@ -44,14 +44,11 @@ export default function page({ params }) {
           {item.youtubeId && (
             <div className={styles.iframeContainer}>
               <iframe
-                width="560"
-                height="315"
                 src={"https://www.youtube.com/embed/" + item.youtubeId}
                 title={New.slug}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
-                loading="lazy"
               ></iframe>
             </div>
           )}
