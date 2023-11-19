23eb9ef62e24a28144789ea85b1d9dd7b685919e
hassanelq <hassanelqadi2@gmail.com>
2023-11-19 20:17:27 +0100

    Update responsive design in NavBar and fix LinkedIn links in members.js

--- app/Database/members.js
+++ app/Database/members.js
@@ -5,7 +5,7 @@ export const members = [
     name: "Mouad Benlamine",
     Image: members_pics.Mouad,
     post: "President",
-    linkedin:'https://www.linkedin.com/in/mouad-benlamine-002671201/',
+    linkedin: "https://www.linkedin.com/in/mouad-benlamine-002671201/",
     description:
       "If your actions inspire others to dream more, and become more, you are a leader.",
   },
@@ -13,7 +13,7 @@ export const members = [
     name: "Mohamed El Founani",
     Image: members_pics.Founani,
     post: "Vice-President",
-    linkedin:'https://www.linkedin.com/in/mohamed-euler-founani/',
+    linkedin: "https://www.linkedin.com/in/mohamed-euler-founani/",
     description:
       "life's a set of gears ⚙. You can have high torque🔧, but low horsepower 🏇",
   },
@@ -21,15 +21,14 @@ export const members = [
     name: "Ayoub Bahti",
     Image: members_pics.Ayoub,
     post: "Treasurer",
-    linkedin:'https://www.linkedin.com/in/ayoub-bahti/',
-    description:
-      "Opportunities don’t happen .you create them.",
+    linkedin: "https://www.linkedin.com/in/ayoub-bahti/",
+    description: "Opportunities don’t happen .you create them.",
   },
   {
     name: "Abdelhamid ElBannani",
     Image: members_pics.Abdelhamid,
     post: "General secretary",
-    linkedin:'https://www.linkedin.com/in/abdelhamid-el-bannani/',
+    linkedin: "https://www.linkedin.com/in/abdelhamid-el-bannani/",
     description:
       "وَٱلْكَاتبينَ بسُمْر ٱلْخطِّ مَا تركَت أقْلامُهُمْ حرفَ جسْمٍ غَيْرمُنْعَجمِ",
   },
@@ -37,7 +36,7 @@ export const members = [
     name: "Manal Attik",
     Image: members_pics.Attik,
     post: "Head of Int/Ext Relations",
-    linkedin:'https://www.linkedin.com/in/manal-attik-71b492279/',
+    linkedin: "https://www.linkedin.com/in/manal-attik-71b492279/",
     description:
       "Passion is energy. Feel the power that comes from focusing on what excites you.",
   },
@@ -45,15 +44,14 @@ export const members = [
     name: "Mohamed Baroudi",
     Image: members_pics.Baroudi,
     post: "Head of Logistics",
-    linkedin:'https://www.linkedin.com/in/medbaroudi/',
-    description:
-      "The magic you are looking for is in the work you're avoiding",
+    linkedin: "https://www.linkedin.com/in/medbaroudi/",
+    description: "The magic you are looking for is in the work you're avoiding",
   },
   {
     name: "Imad Aouragh",
     Image: members_pics.Imad,
     post: "Team coordinator",
-    linkedin:'https://www.linkedin.com/in/imad-aouragh-496607221/',
+    linkedin: "https://www.linkedin.com/in/imad-aouragh-496607221/",
     description:
       "Education is a weapon, whose effect depends on who holds it in his hands and at whom it is aimed",
   },
@@ -61,7 +59,7 @@ export const members = [
     name: "Khalid Et-tazy",
     Image: members_pics.Khalid,
     post: "Event Manager",
-    linkedin:'https://www.linkedin.com/in/khalid-et-tazy-860a741b6/',
+    linkedin: "https://www.linkedin.com/in/khalid-et-tazy-860a741b6/",
     description:
       "Se réunir est un début, rester ensemble est un progrès, travailler ensemble est la réussite.",
   },
@@ -69,7 +67,7 @@ export const members = [
     name: "Samira Errai",
     Image: members_pics.Samira,
     post: "Clubs Coordinator",
-    linkedin:'https://www.linkedin.com/in/samira-errai-0656b2184/',
+    linkedin: "https://www.linkedin.com/in/samira-errai-0656b2184/",
     description:
       "It is hard work, perseverance, sacrifice, and a genuine love for what you do. Not luck!",
   },
@@ -77,15 +75,14 @@ export const members = [
     name: "Hassan elqadi",
     Image: members_pics.Hassan,
     post: "Head of Innovation and IT",
-    linkedin:'https://www.linkedin.com/in/el-qadi/',
-    description:
-      "UI is the saddle, the stirrups, & the reins. UX is the feeling you get being able to ride the horse.",
+    linkedin: "https://www.linkedin.com/in/el-qadi/",
+    description: "Be the architect of change, not a mimic of tradition.",
   },
   {
     name: "Ibtissam Massa",
     Image: members_pics.Ibtissam,
     post: "Student Affairs Coordinator",
-    linkedin:'https://www.linkedin.com/in/ibtissam-massa/',
+    linkedin: "https://www.linkedin.com/in/ibtissam-massa/",
     description:
       "UI is the saddle, the stirrups, & the reins. UX is the feeling you get being able to ride the horse.",
   },
@@ -93,23 +90,21 @@ export const members = [
     name: "Zakaria Ahrbil",
     Image: members_pics.zakaria,
     post: "Designer and SM",
-    linkedin:'https://www.linkedin.com/in/zakaria-ahrbil-772041236/',
-    description:
-      "UI is the saddle, the stirrups, & the reins. UX is the feeling you get being able to ride the horse.",
+    linkedin: "https://www.linkedin.com/in/zakaria-ahrbil-772041236/",
+    description: "I have no enemies.",
   },
   {
     name: "Wissal Benider",
     Image: members_pics.Wissal,
     post: "Head of communication",
-    linkedin:'https://www.linkedin.com/in/wissal-benider-578471221/',
-    description:
-      "life begins at the end of your comfort zone",
+    linkedin: "https://www.linkedin.com/in/wissal-benider-578471221/",
+    description: "life begins at the end of your comfort zone",
   },
   {
     name: "Malak Bourrich",
     Image: members_pics.Malak,
     post: "Head of sponsoring",
-    linkedin:'https://www.linkedin.com/in/malak-bourrich-6a5193249/',
+    linkedin: "https://www.linkedin.com/in/malak-bourrich-6a5193249/",
     description:
       "Strategic Sponsorship Manager : Expert in driving partnerships.",
   },
@@ -117,7 +112,7 @@ export const members = [
     name: "Mehdi Abouelmouahib",
     Image: members_pics.Mehdi,
     post: "Assistant Member",
-    linkedin:'https://www.linkedin.com/in/mehdi-abouelmouahib-52b161254/',
+    linkedin: "https://www.linkedin.com/in/mehdi-abouelmouahib-52b161254/",
     description:
       "Like lines of code 👨🏻‍💻, the choices we make in life determine the output we receive. Choose Wisely 🧙🏼‍♂️!",
   },
@@ -125,7 +120,7 @@ export const members = [
     name: "Somaya Arouch",
     Image: members_pics.Somaya,
     post: "Assistant Member",
-    linkedin:'https://www.linkedin.com/in/somaya-arouch-889982265/',
+    linkedin: "https://www.linkedin.com/in/somaya-arouch-889982265/",
     description:
       "A lot of people tell me I’m a bit dreamy. But I like the idea of that. Of being somewhere else.",
   },
@@ -133,7 +128,7 @@ export const members = [
     name: "Salma Mahmoud",
     Image: members_pics.Salma,
     post: "Assistant Member",
-    linkedin:'https://www.linkedin.com/in/salma-mahmoud-981719230/',
+    linkedin: "https://www.linkedin.com/in/salma-mahmoud-981719230/",
     description:
       "Team work is the art of harmonizing diverse talents towards a common vision",
   },
