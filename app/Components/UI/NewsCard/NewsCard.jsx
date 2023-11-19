223cb3ad9d0cb66e37b9a2c35c376ca5d1b0ce2b
hassanelq <hassanelqadi2@gmail.com>
2023-11-19 17:13:05 +0100

    Update gap values in CSS files

--- app/Components/UI/NewsCard/NewsCard.jsx
+++ app/Components/UI/NewsCard/NewsCard.jsx
@@ -2,9 +2,20 @@ import Image from "next/image";
 import styles from "./NewsCard.module.css";
 import Link from "next/link";
 
-export default function NewsCard({ slug, title, category, description, img }) {
+export default function NewsCard({
+  slug,
+  title,
+  category,
+  description,
+  img,
+  direction,
+}) {
   return (
-    <Link href={"/News/" + category + "/" + slug} className={styles.card}>
+    <Link
+      href={"/News/" + category + "/" + slug}
+      style={{ flexDirection: direction }}
+      className={styles.card}
+    >
       <div className={styles.imgContainer}>
         <Image src={img} alt={slug} className={styles.img} />
       </div>
--- app/News/page.jsx
+++ app/News/page.jsx
@@ -21,6 +21,7 @@ export default function page() {
         description={item.description}
         img={item.coverimg}
         slug={item.slug}
+        direction="row"
       />
     ));
 
