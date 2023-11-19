97aebd1a89e5630e59b96c4bf4610573b1aa02e7
ibtissammassa <bettysama16@gmail.com>
2023-11-19 23:44:10 +0100

    Update meta tag for Twitter image

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta property="og:image" content="<generated>" />
-        <meta name="facebook:image" content="<generated>" />
+        <meta name="twitter:image" content="<generated>" />
       </Head>
       <body>
         <NavBar />
