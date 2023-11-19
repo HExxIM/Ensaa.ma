2d3dfd9f0065a832b377ccc332beeb86e291d3c3
ibtissammassa <bettysama16@gmail.com>
2023-11-19 23:56:52 +0100

    Update meta tags for social media sharing

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,10 +27,13 @@ export default function RootLayout({ children }) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta property="og:image" content="<generated>" />
-        <meta name="twitter:image" content="<generated>" />
         <meta property="og:image:type" content="<generated>" />
         <meta property="og:image:width" content="<generated>" />
         <meta property="og:image:height" content="<generated>" />
+        <meta name="twitter:image" content="<generated>" />
+        <meta name="twitter:image:type" content="<generated>" />
+        <meta name="twitter:image:width" content="<generated>" />
+        <meta name="twitter:image:height" content="<generated>" />
       </Head>
       <body>
         <NavBar />
