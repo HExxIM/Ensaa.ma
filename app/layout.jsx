6c68462cb81c098a6ca52e39f9307a03c16953c1
ibtissammassa <bettysama16@gmail.com>
2023-11-19 23:54:59 +0100

    Add meta tags for social media sharing

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,6 +28,9 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta property="og:image" content="<generated>" />
         <meta name="twitter:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
       </Head>
       <body>
         <NavBar />
