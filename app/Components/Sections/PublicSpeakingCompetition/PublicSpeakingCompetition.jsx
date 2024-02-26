e8e5b9e507f2120cc53e649218bdf4cd2259ed43
hassanelq <hassanelqadi2@gmail.com>
2024-02-26 21:24:09 +0100

    Refactor PublicSpeakingCompetition component

--- app/Components/Sections/PublicSpeakingCompetition/PublicSpeakingCompetition.jsx
+++ app/Components/Sections/PublicSpeakingCompetition/PublicSpeakingCompetition.jsx
@@ -1,46 +1,76 @@
-import styles from './page.module.css'
-import SocialMediaList from '../../UI/SocialMediaList';
-import participants from '../../../Database/PublicSpeakingCompetition'
-import Image from 'next/image';
+import styles from "./page.module.css";
+import SocialMediaList from "../../UI/SocialMediaList";
+import participants from "../../../Database/PublicSpeakingCompetition";
+import Image from "next/image";
 
 export const metadata = {
-    title: "Ensaa - PublicSpeakingCompetition",
-    description: "ADE Ensaa - PublicSpeakingCompetition",
+  title: "Ensaa - PublicSpeakingCompetition",
+  description: "ADE Ensaa - PublicSpeakingCompetition",
 };
 
 function PublicSpeakingCompetition() {
-    const links = {
-        facebook:"https://www.facebook.com/photo/?fbid=788434646635668&set=a.454223453390124/",
-        instagram: "https://www.instagram.com/p/C3GVkUvsNZU/",
-      }
-    return ( 
-        <div className={styles.container}>
-            <h1>L’ingénieur ENSAA prend la parole</h1>
-            <div className={styles.infos}>
-                <span>Event</span>
-                <SocialMediaList
-                    direction="row"
-                    className={styles.SocialMediaList}
-                    links={links}
-                    iconsSize="23"
-                    iconsGap="1.2rem"
-                    brightness="1"
-                />
-            </div>  
-            <p>Découvrez les participants du concours L’INGÉNIEUR ENSAA PREND LA PAROLE ! Chaque image représente un individu talentueux prêt à mettre en valeur ses compétences. Gardez un œil sur les participants éliminés que nous marquons en niveau de gris. Qui émergera victorieux et prendra la parole ?</p>
-            <div className={styles.participants}>
-                {
-                    participants.map((participant, index) => {
-                        return (
-                            <div key={index} className={participant.in==true ? styles.participant : styles.out}>
-                                <Image className={styles.img} src={participant.img} alt={participant.name}/>
-                            </div>
-                        )
-                    })
-                }
+  const links = {
+    facebook:
+      "https://www.facebook.com/photo/?fbid=788434646635668&set=a.454223453390124/",
+    instagram: "https://www.instagram.com/p/C3GVkUvsNZU/",
+  };
+  return (
+    <div className={styles.container}>
+      <h1>L’ingénieur ENSAA prend la parole</h1>
+      <div className={styles.infos}>
+        <span>Event</span>
+        <SocialMediaList
+          direction="row"
+          className={styles.SocialMediaList}
+          links={links}
+          iconsSize="23"
+          iconsGap="1.2rem"
+          brightness="1"
+        />
+      </div>
+      <p>
+        Découvrez les participants du concours L’INGÉNIEUR ENSAA PREND LA PAROLE
+        ! Chaque image représente un individu talentueux prêt à mettre en valeur
+        ses compétences. Gardez un œil sur les participants éliminés que nous
+        marquons en niveau de gris. Qui émergera victorieux et prendra la parole
+        ?
+      </p>
+      <div className={styles.participants}>
+        {participants[0].map((participant, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                participant.in == true ? styles.participant : styles.out
+              }
+            >
+              <Image
+                className={styles.img}
+                src={participant.img}
+                alt={participant.name}
+              />
             </div>
-        </div>
-     );
+          );
+        })}
+        {participants[1].map((participant, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                participant.in == true ? styles.participant : styles.out
+              }
+            >
+              <Image
+                className={styles.img}
+                src={participant.img}
+                alt={participant.name}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default PublicSpeakingCompetition;
--- app/Database/PublicSpeakingCompetition.js
+++ app/Database/PublicSpeakingCompetition.js
@@ -30,168 +30,371 @@ import cond29 from "../../public/images/News/condidats/29.PNG";
 import cond30 from "../../public/images/News/condidats/30.PNG";
 import cond31 from "../../public/images/News/condidats/31.PNG";
 
+// const participants = [
+//   [
+//     {
+//       name: "Anonwodji Kodjovi Daniel",
+//       img: cond1,
+//       in: true,
+//       slug: "Anonwodji-Kodjovi-Daniel",
+//     },
+//     {
+//       name: "Bakir Bassma",
+//       img: cond4,
+//       in: true,
+//     },
+//     {
+//       name: "Arssali Chaimae",
+//       img: cond6,
+//       in: true,
+//     },
+//     {
+//       name: "MOUMEN Assia",
+//       img: cond24,
+//       in: true,
+//     },
+//     {
+//       name: "EL HARRAZ Hafsa",
+//       img: cond26,
+//       in: true,
+//     },
+//   ],
+//   [
+//     {
+//       name: "Imich Achraf",
+//       img: cond3,
+//       in: false,
+//     },
+
+//     {
+//       name: "Chakir Douae",
+//       img: cond2,
+//       in: false,
+//     },
+
+//     {
+//       name: "Amemastron Yaw Luc",
+//       img: cond5,
+//       in: false,
+//     },
+
+//     {
+//       name: "FIAVOR Armel Kossivi",
+//       img: cond7,
+//       in: false,
+//     },
+//     {
+//       name: "OUIHLANE Zhour",
+//       img: cond8,
+//       in: false,
+//     },
+//     {
+//       name: "ABDESSAMAD Elmouatassim",
+//       img: cond9,
+//       in: false,
+//     },
+//     {
+//       name: "LAFTOUHI Wissal",
+//       img: cond10,
+//       in: false,
+//     },
+//     {
+//       name: "TIHAOUI Abdelfattah",
+//       img: cond11,
+//       in: false,
+//     },
+//     {
+//       name: "MOUSLIMO Elmoukhtar",
+//       img: cond12,
+//       in: false,
+//     },
+//     {
+//       name: "HOUBBANE Salma",
+//       img: cond13,
+//       in: false,
+//     },
+//     {
+//       name: "LATIF Hayat",
+//       img: cond14,
+//       in: false,
+//     },
+//     {
+//       name: "EL MOURTAJI Ali",
+//       img: cond15,
+//       in: false,
+//     },
+//     {
+//       name: "BRIERE Hamza",
+//       img: cond16,
+//       in: false,
+//     },
+//     {
+//       name: "NACIR Nizar",
+//       img: cond17,
+//       in: false,
+//     },
+//     {
+//       name: "ELHASSANI Youssef",
+//       img: cond18,
+//       in: false,
+//     },
+//     {
+//       name: "ABHRY Mohammed",
+//       img: cond19,
+//       in: false,
+//     },
+//     {
+//       name: "DBIB Mariam",
+//       img: cond20,
+//       in: false,
+//     },
+//     {
+//       name: "BAYAD Safa",
+//       img: cond21,
+//       in: false,
+//     },
+//     {
+//       name: "MANSOURI Fatima Zahra",
+//       img: cond22,
+//       in: false,
+//     },
+//     {
+//       name: "EL KHADIM Lahcen",
+//       img: cond23,
+//       in: false,
+//     },
+
+//     {
+//       name: "BADI Sanae",
+//       img: cond25,
+//       in: false,
+//     },
+
+//     {
+//       name: "KHARBOUCH Khadija",
+//       img: cond27,
+//       in: false,
+//     },
+//     {
+//       name: "BOURKHA Hassna",
+//       img: cond28,
+//       in: false,
+//     },
+//     {
+//       name: "LAADIMI Mohammed",
+//       img: cond29,
+//       in: false,
+//     },
+//     {
+//       name: "BAKARI Meriem",
+//       img: cond30,
+//       in: false,
+//     },
+//     {
+//       name: "LOUHAM Nouhaila",
+//       img: cond31,
+//       in: false,
+//     },
+//   ],
+// ];
+
+//create the same data adding slug to each participant
 const participants = [
-  {
-    name: "Anonwodji Kodjovi Daniel",
-    img: cond2,
-    in: true,
-  },
-  {
-    name: "Bakir Bassma",
-    img: cond4,
-    in: true,
-  },
-  {
-    name: "Arssali Chaimae",
-    img: cond6,
-    in: true,
-  },
-  {
-    name: "MOUMEN Assia",
-    img: cond24,
-    in: true,
-  },
-  {
-    name: "EL HARRAZ Hafsa",
-    img: cond26,
-    in: true,
-  },
+  [
+    {
+      name: "Anonwodji Kodjovi Daniel",
+      img: cond1,
+      in: true,
+      slug: "Anonwodji-Kodjovi-Daniel",
+    },
+    {
+      name: "Bakir Bassma",
+      img: cond4,
+      in: true,
+      slug: "Bakir-Bassma",
+    },
+    {
+      name: "Arssali Chaimae",
+      img: cond6,
+      in: true,
+      slug: "Arssali-Chaimae",
+    },
+    {
+      name: "MOUMEN Assia",
+      img: cond24,
+      in: true,
+      slug: "MOUMEN-Assia",
+    },
+    {
+      name: "EL HARRAZ Hafsa",
+      img: cond26,
+      in: true,
+      slug: "EL-HARRAZ-Hafsa",
+    },
+  ],
+  [
+    {
+      name: "Imich Achraf",
+      img: cond3,
+      in: false,
+      slug: "Imich-Achraf",
+    },
 
-  {
-    name: "Imich Achraf",
-    img: cond1,
-    in: false,
-  },
+    {
+      name: "Chakir Douae",
+      img: cond2,
+      in: false,
+      slug: "Chakir-Douae",
+    },
 
-  {
-    name: "Chakir Douae",
-    img: cond3,
-    in: false,
-  },
+    {
+      name: "Amemastron Yaw Luc",
+      img: cond5,
+      in: false,
+      slug: "Amemastron-Yaw-Luc",
+    },
 
-  {
-    name: "Amemastron Yaw Luc",
-    img: cond5,
-    in: false,
-  },
+    {
+      name: "FIAVOR Armel Kossivi",
+      img: cond7,
+      in: false,
+      slug: "FIAVOR-Armel-Kossivi",
+    },
+    {
+      name: "OUIHLANE Zhour",
+      img: cond8,
+      in: false,
+      slug: "OUIHLANE-Zhour",
+    },
+    {
+      name: "ABDESSAMAD Elmouatassim",
+      img: cond9,
+      in: false,
+      slug: "ABDESSAMAD-Elmouatassim",
+    },
+    {
+      name: "LAFTOUHI Wissal",
+      img: cond10,
+      in: false,
+      slug: "LAFTOUHI-Wissal",
+    },
+    {
+      name: "TIHAOUI Abdelfattah",
+      img: cond11,
+      in: false,
+      slug: "TIHAOUI-Abdelfattah",
+    },
+    {
+      name: "MOUSLIMO Elmoukhtar",
+      img: cond12,
+      in: false,
+      slug: "MOUSLIMO-Elmoukhtar",
+    },
+    {
+      name: "HOUBBANE Salma",
+      img: cond13,
+      in: false,
+      slug: "HOUBBANE-Salma",
+    },
+    {
+      name: "LATIF Hayat",
+      img: cond14,
+      in: false,
+      slug: "LATIF-Hayat",
+    },
+    {
+      name: "EL MOURTAJI Ali",
+      img: cond15,
+      in: false,
+      slug: "EL-MOURTAJI-Ali",
+    },
+    {
+      name: "BRIERE Hamza",
+      img: cond16,
+      in: false,
+      slug: "BRIERE-Hamza",
+    },
+    {
+      name: "NACIR Nizar",
+      img: cond17,
+      in: false,
+      slug: "NACIR-Nizar",
+    },
+    {
+      name: "ELHASSANI Youssef",
+      img: cond18,
+      in: false,
+      slug: "ELHASSANI-Youssef",
+    },
+    {
+      name: "ABHRY Mohammed",
+      img: cond19,
+      in: false,
+      slug: "ABHRY-Mohammed",
+    },
+    {
+      name: "DBIB Mariam",
+      img: cond20,
+      in: false,
+      slug: "DBIB-Mariam",
+    },
+    {
+      name: "BAYAD Safa",
+      img: cond21,
+      in: false,
+      slug: "BAYAD-Safa",
+    },
+    {
+      name: "MANSOURI Fatima Zahra",
+      img: cond22,
+      in: false,
+      slug: "MANSOURI-Fatima-Zahra",
+    },
+    {
+      name: "EL KHADIM Lahcen",
+      img: cond23,
+      in: false,
+      slug: "EL-KHADIM-Lahcen",
+    },
 
-  {
-    name: "FIAVOR Armel Kossivi",
-    img: cond7,
-    in: false,
-  },
-  {
-    name: "OUIHLANE Zhour",
-    img: cond8,
-    in: false,
-  },
-  {
-    name: "ABDESSAMAD Elmouatassim",
-    img: cond9,
-    in: false,
-  },
-  {
-    name: "LAFTOUHI Wissal",
-    img: cond10,
-    in: false,
-  },
-  {
-    name: "TIHAOUI Abdelfattah",
-    img: cond11,
-    in: false,
-  },
-  {
-    name: "MOUSLIMO Elmoukhtar",
-    img: cond12,
-    in: false,
-  },
-  {
-    name: "HOUBBANE Salma",
-    img: cond13,
-    in: false,
-  },
-  {
-    name: "LATIF Hayat",
-    img: cond14,
-    in: false,
-  },
-  {
-    name: "EL MOURTAJI Ali",
-    img: cond15,
-    in: false,
-  },
-  {
-    name: "BRIERE Hamza",
-    img: cond16,
-    in: false,
-  },
-  {
-    name: "NACIR Nizar",
-    img: cond17,
-    in: false,
-  },
-  {
-    name: "ELHASSANI Youssef",
-    img: cond18,
-    in: false,
-  },
-  {
-    name: "ABHRY Mohammed",
-    img: cond19,
-    in: false,
-  },
-  {
-    name: "DBIB Mariam",
-    img: cond20,
-    in: false,
-  },
-  {
-    name: "BAYAD Safa",
-    img: cond21,
-    in: false,
-  },
-  {
-    name: "MANSOURI Fatima Zahra",
-    img: cond22,
-    in: false,
-  },
-  {
-    name: "EL KHADIM Lahcen",
-    img: cond23,
-    in: false,
-  },
+    {
+      name: "BADI Sanae",
+      img: cond25,
+      in: false,
+      slug: "BADI-Sanae",
+    },
 
-  {
-    name: "BADI Sanae",
-    img: cond25,
-    in: false,
-  },
-
-  {
-    name: "KHARBOUCH Khadija",
-    img: cond27,
-    in: false,
-  },
-  {
-    name: "BOURKHA Hassna",
-    img: cond28,
-    in: false,
-  },
-  {
-    name: "LAADIMI Mohammed",
-    img: cond29,
-    in: false,
-  },
-  {
-    name: "BAKARI Meriem",
-    img: cond30,
-    in: false,
-  },
-  {
-    name: "LOUHAM Nouhaila",
-    img: cond31,
-    in: false,
-  },
+    {
+      name: "KHARBOUCH Khadija",
+      img: cond27,
+      in: false,
+      slug: "KHARBOUCH-Khadija",
+    },
+    {
+      name: "BOURKHA Hassna",
+      img: cond28,
+      in: false,
+      slug: "BOURKHA-Hassna",
+    },
+    {
+      name: "LAADIMI Mohammed",
+      img: cond29,
+      in: false,
+      slug: "LAADIMI-Mohammed",
+    },
+    {
+      name: "BAKARI Meriem",
+      img: cond30,
+      in: false,
+      slug: "BAKARI-Meriem",
+    },
+    {
+      name: "LOUHAM Nouhaila",
+      img: cond31,
+      in: false,
+      slug: "LOUHAM-Nouhaila",
+    },
+  ],
 ];
 
 export default participants;
