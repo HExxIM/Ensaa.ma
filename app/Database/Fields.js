629498067ba58825bc6c6c0ca4eaf882389d5404
hassanelq <hassanelqadi2@gmail.com>
2024-04-30 00:33:15 +0100

    Refactor Fields page component and update metadata

--- app/Database/Fields.js
+++ app/Database/Fields.js
@@ -2,55 +2,106 @@ import btp from "../../public/images/fields/BTP.png";
 import info from "../../public/images/fields/Info.png";
 import China from "../../public/images/fields/China.png";
 import Fid from "../../public/images/fields/FID.png";
+import gee from "../../public/images/fields/gee.png";
+import indus from "../../public/images/fields/indus.png";
+import ge from "../../public/images/fields/ge.png";
+import gm from "../../public/images/fields/gm.png";
 
 export default [
-    {
-      title: "Civil Engineering (BTP)",
-      slug: "btp",
-      description:
-        "Are you curious enough to understand the fundamental principles of designing, constructing, and maintaining the infrastructures that surround us? Do skyscrapers, roads, and works of art resonate with you every time you lay eyes on them? We kick off the 'ingénierie bla filtre' conference series with one of the oldest branches of engineering, civil engineering. To delve into a world of creativity, science, and technology, we invite our esteemed guest Mohammed Ali Chekka, a graduate of ENSA-A, currently working as a construction technical control engineer and freelancing in structural calculations in BA & CM.",
-      coverimg: btp,
-      content: [
-        {
-          youtubeId: "_7FeIse14cM",
-        },
-      ],
-    },
-    {
-      title: "Software engineering",
-      slug: "informatique",
-      description:
-        "We delved into the world of computer engineering, where innovation and technology intersected. We had the pleasure of hosting Imad Eddine Fouad, software engineer at Société Générale CIB, and Mohamed EL ATTAR, engineer at SQLI. They provided us with an overview of their journeys and opportunities in the field of computer science.",
-      coverimg: info,
-      content: [
-        {
-          youtubeId: "fAkF3hZJoF8",
-        },
-      ],
-    },
-    {
-      title: "Study abroad (China)",
-      slug: "chine",
-      description:
-        "A special bonus where Ibrahim CHENNAH and Safa DOUH, educational consultants, shared their knowledge about studying in China. They explored how the educational process unfolds, providing valuable insights for those interested in the academic experience abroad.",
-      coverimg: China,
-      content: [
-        {
-          youtubeId: "xVyf1QCryKk",
-        },
-      ],
-    },
-    {
-      title: "Finance and Decision Engineering (FID)",
-      slug: "fid",
-      description:
-        "This session is dedicated to Finance and Decision Engineering, featuring Mohamed Hicham Echaoui, Valuation and P&L Analyst at ENGIE. An episode that promises to shed light on the converging paths of finance and engineering, without delving into technical details, but with the guarantee of an exciting exploration. An opportunity not to be missed to enrich your understanding and stimulate your curiosity in this dynamic field.",
-      coverimg: Fid,
-      content: [
-        {
-          youtubeId: "Coq_70LJn_E",
-        },
-      ],
-    },
-  ];
-  
+  {
+    title: "Civil Engineering (BTP)",
+    slug: "btp",
+    description:
+      "Are you curious enough to understand the fundamental principles of designing, constructing, and maintaining the infrastructures that surround us? Do skyscrapers, roads, and works of art resonate with you every time you lay eyes on them? We kick off the 'ingénierie bla filtre' conference series with one of the oldest branches of engineering, civil engineering. To delve into a world of creativity, science, and technology, we invite our esteemed guest Mohammed Ali Chekka, a graduate of ENSA-A, currently working as a construction technical control engineer and freelancing in structural calculations in BA & CM.",
+    coverimg: btp,
+    content: [
+      {
+        youtubeId: "_7FeIse14cM",
+      },
+    ],
+  },
+  {
+    title: "Software engineering",
+    slug: "informatique",
+    description:
+      "We delved into the world of computer engineering, where innovation and technology intersected. We had the pleasure of hosting Imad Eddine Fouad, software engineer at Société Générale CIB, and Mohamed EL ATTAR, engineer at SQLI. They provided us with an overview of their journeys and opportunities in the field of computer science.",
+    coverimg: info,
+    content: [
+      {
+        youtubeId: "fAkF3hZJoF8",
+      },
+    ],
+  },
+  {
+    title: "Study abroad (China)",
+    slug: "chine",
+    description:
+      "A special bonus where Ibrahim CHENNAH and Safa DOUH, educational consultants, shared their knowledge about studying in China. They explored how the educational process unfolds, providing valuable insights for those interested in the academic experience abroad.",
+    coverimg: China,
+    content: [
+      {
+        youtubeId: "xVyf1QCryKk",
+      },
+    ],
+  },
+  {
+    title: "Finance and Decision Engineering (FID)",
+    slug: "fid",
+    description:
+      "This session is dedicated to Finance and Decision Engineering, featuring Mohamed Hicham Echaoui, Valuation and P&L Analyst at ENGIE. An episode that promises to shed light on the converging paths of finance and engineering, without delving into technical details, but with the guarantee of an exciting exploration. An opportunity not to be missed to enrich your understanding and stimulate your curiosity in this dynamic field.",
+    coverimg: Fid,
+    content: [
+      {
+        youtubeId: "Coq_70LJn_E",
+      },
+    ],
+  },
+  {
+    title: "Génie des procédés, de l'énergie et de l'environnement (GEE)",
+    slug: "gee",
+    description:
+      "Cette session de L'Ingénierie Bla Filtre est dédiée au Génie de Procédés de l’Energie et de l’Environnement, avec Amine Benkarroum, étudiant en double diplôme en Ingénierie : Processus, Énergie et Environnement - Gestion des Risques. Une séance qui promet de jeter un pont entre le génie des procédés et la gestion de l'environnement, offrant une exploration captivante sans s'embourber dans les détails techniques. Une occasion incontournable pour approfondir votre compréhension et éveiller votre curiosité dans ce domaine crucial et dynamique.",
+    coverimg: gee,
+    content: [
+      {
+        youtubeId: "Qm1oxnqRUk8",
+      },
+    ],
+  },
+  {
+    title: "Génie Industriel (GI)",
+    slug: "gi",
+    description:
+      "Cette session de L'Ingénierie Bla Filtre, dédiée au Génie Industriel, avec Oussama Eddari, Supply Chain Manager et Demand Planner chez IWASE COSFA EUROPE. Une séance enrichissante qui fera le lien entre le génie industriel et les aspects pratiques de la gestion de la chaîne d'approvisionnement. Une excellente occasion pour élargir votre compréhension et stimuler votre curiosité dans ce secteur vital et en constante évolution.",
+    coverimg: indus,
+    content: [
+      {
+        youtubeId: "bK-WnJ791io",
+      },
+    ],
+  },
+  {
+    title: "Génie électrique (GE)",
+    slug: "ge",
+    description:
+      "Une nouvelle session de L'Ingénierie Bla Filtre, consacrée cette fois au Génie Électrique, avec Hajar Ram QAIBOU, ingénieure logicielle certifiée en Kubernetes. Préparez-vous à explorer les intersections entre le génie électrique et les technologies modernes de gestion de conteneurs. Une occasion parfaite pour approfondir vos connaissances et éveiller votre intérêt pour ce domaine crucial et innovant.",
+    coverimg: ge,
+    content: [
+      {
+        youtubeId: "e3wH6XYF53c",
+      },
+    ],
+  },
+  {
+    title: "Génie mécanique (GM)",
+    slug: "gm",
+    description:
+      "Cette session de L'Ingénierie Bla Filtre, est axée sur le Génie Mécanique, avec Nouhaila HAGGOUCHI, Ingénieur synthèse implantation chez MG2 Engineering (une coentreprise entre MAGNA & CAPGEMINI ENGINEERING). Découvrir comment les principes du génie mécanique s'intègrent dans les processus modernes d'implantation et de synthèse industrielle. Ne manquez pas cette opportunité d'enrichir votre compréhension et de nourrir votre curiosité dans ce secteur dynamique et essentiel.",
+    coverimg: gm,
+    content: [
+      {
+        youtubeId: "HKmVDrRFifY",
+      },
+    ],
+  },
+];
--- app/Fields/[slug]/page.jsx
+++ app/Fields/[slug]/page.jsx
@@ -4,46 +4,49 @@ import Image from "next/image";
 import Fields from "../../Database/Fields.js";
 
 export const metadata = {
-    description: "Explore the various fields of study offered at ENSAA.",
-    keywords: "ADE, Dreamers, ENSAA, fields, study, engineering, specialization",
+  description: "Explore the various fields of study offered at ENSAA.",
+  keywords: "ADE, Dreamers, ENSAA, fields, study, engineering, specialization",
 };
 
 function page({ params }) {
-    const slug = params.slug;
-    metadata.title = `ENSAA - Fields - ${slug}`;
-    const Field = Fields.find((item) => item.slug === slug);
-    return (
-        <div className={styles.container}>
-            <h1>{Field.title}</h1>
-            <p>{Field.description}</p>
-        {Field.content && Field.content.map((item, index) => (
-        <>
-          {item.text && <p dangerouslySetInnerHTML={{__html: item.text}}></p>}
-          {item.img && (
-            <div className={styles.imgContainer}>
-              <Image
-                placeholder="blur"
-                className={styles.img}
-                alt={slug}
-                src={item.img}
-              />
-            </div>
-          )}
-          {item.youtubeId && (
-            <div className={styles.iframeContainer}>
-              <iframe
-                src={"https://www.youtube.com/embed/" + item.youtubeId}
-                title={Field.slug}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
-            </div>
-          )}
-        </>
-      ))}
-        </div>
-     );
+  const slug = params.slug;
+  metadata.title = `ENSAA - Fields - ${slug}`;
+  const Field = Fields.find((item) => item.slug === slug);
+  return (
+    <div className={styles.container}>
+      <h1>{Field.title}</h1>
+      <p>{Field.description}</p>
+      {Field.content &&
+        Field.content.map((item, index) => (
+          <>
+            {item.text && (
+              <p dangerouslySetInnerHTML={{ __html: item.text }}></p>
+            )}
+            {item.img && (
+              <div className={styles.imgContainer}>
+                <Image
+                  placeholder="blur"
+                  className={styles.img}
+                  alt={slug}
+                  src={item.img}
+                />
+              </div>
+            )}
+            {item.youtubeId && (
+              <div className={styles.iframeContainer}>
+                <iframe
+                  src={"https://www.youtube.com/embed/" + item.youtubeId}
+                  title={Field.slug}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            )}
+          </>
+        ))}
+    </div>
+  );
 }
 
 export default page;
