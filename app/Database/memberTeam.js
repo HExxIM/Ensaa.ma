67de2ef29799c120fd72bfbb39ec289dbd71cfa9
hassan_elq <hassanelqadi2@gmail.com>
2025-08-29 18:43:13 +0100

    bde members

--- app/Database/memberTeam.js
+++ app/Database/memberTeam.js
@@ -3,11 +3,138 @@ import members_picsDreamers from "../constants/members_pics";
 const membersIgniters = [
   {
     name: "Mehdi Abouelmouahib",
-    Image: members_picsDreamers.Mehdi,
+    Image: "/images/Igniters/Mehdi.jpg",
     post: "President",
     linkedin: "https://www.linkedin.com/in/mehdi-abouelmouahib-52b161254/",
+    description: "No matter how deep the stack is, I overflow with purpose!",
+  },
+  {
+    name: "Hayat Latif",
+    Image: "/images/Igniters/Hayat.jpeg",
+    post: "Vice-Président",
+    linkedin: "https://www.linkedin.com/in/hayat-latif-a459ab24b/",
     description:
-      "Like lines of code 👨🏻‍💻, the choices we make in life determine the output we receive. Choose Wisely 🧙🏼‍♂️!",
+      "Growing patiently step by step, embodying the values wished from the world, and trusting communication as the key to every challenge!",
+  },
+  {
+    name: "Hiba Eljayyi ",
+    Image: "/images/Igniters/Hiba_Loughzal.png",
+    post: "Secrétaire général",
+    linkedin:
+      "https://www.linkedin.com/in/hiba-eljayyi-0094b8315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    description:
+      "Never settle for half answers, see every challenge as a process to master, every problem as energy to transform!",
+  },
+  {
+    name: "Khalil Hardouf",
+    Image: "/images/Igniters/Khalil.jpeg",
+    post: "Trésorier",
+    linkedin: "",
+    description:
+      "Become the best version of yourself, not a copy of someone else!",
+  },
+  {
+    name: "Hiba Loughzal",
+    Image: "/images/Igniters/Hiba_Loughzal.png",
+    post: " Responsable relation interne externe",
+    linkedin: "https://www.linkedin.com/in/hiba-loughzal-3b5414252/",
+    description:
+      "🤝 Building strong connections inside and outside, valuing trust , collaboration and openness , with the belief that communication  is the path to every success!",
+  },
+  {
+    name: "Yassine El Kharouai",
+    Image: "/images/Igniters/Yassine.jpg",
+    post: " Reponsable logistique",
+    linkedin: "https://www.linkedin.com/in/yassine-el-kharouai-7b5417333/",
+    description: "The truth is binary, but the lies are encoded in layers!",
+  },
+  {
+    name: "Ilyass Abdouttalib",
+    Image: "/images/Igniters/Ilyass.jpeg",
+    post: "Responsable des clubs",
+    linkedin: "https://www.linkedin.com/in/ilyass-abdouttalib-b85a07328/",
+    description:
+      "I believe that real change begins from within 🦁 Dreaming alone is not enough — it requires action, knowledge, and patience 💡 Remember; success is the result of consistent effort !",
+  },
+  {
+    name: "Malak Rhalem",
+    Image: "/images/Igniters/Malak.jpg",
+    post: "Responsable événementiel",
+    linkedin: "https://www.linkedin.com/in/malak-rhalem-b6b141282/",
+    description:
+      "Errors teach more than successes, if only they’re debugged properly!",
+  },
+  {
+    name: "Zakariya Jbiri",
+    Image: "/images/Igniters/zakaria.jpg",
+    post: "Responsable affaires estudiantines",
+    linkedin: "https://www.linkedin.com/in/zakarya-jbiri-72415728b/",
+    description:
+      "La vie s'érige tel un bâtiment, si l'on néglige ses fondations, la destruction n'est jamais bien loin!",
+  },
+  {
+    name: "Soufiane El Barji",
+    Image: "/images/Igniters/Soufiane_El-Barji.jpg",
+    post: "Responsable Design",
+    linkedin: "https://www.linkedin.com/in/soufiane-elbarji/",
+    description:
+      "Innovation isn’t just built with code; it’s powered by people, driven by impact, and fueled by imagination!",
+  },
+  {
+    name: "Adam Bouderqa",
+    Image: "/images/Igniters/adam.jpg",
+    post: "Co-Responsable Design",
+    linkedin: "https://www.linkedin.com/in/adam-wings-3b43b1252/",
+    description:
+      "He who binds himself to the world’s recognition loses himself; he who seeks the source finds his true self!",
+  },
+  {
+    name: "Nizar El Haddaoui",
+    Image: "/images/Igniters/Nizar.jpg",
+    post: "Responsable Media",
+    linkedin:
+      " https://www.linkedin.com/in/nizar-el-haddaoui-b97284381?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app ",
+    description:
+      "Next time, ask 'What's the worst that will happen?' Then push yourself a little further than you dare!",
+  },
+  {
+    name: "Mohamed Essadik El Maghraoui",
+    Image: "/images/Igniters/Mohamed.jpeg",
+    post: " Photographer",
+    linkedin:
+      "https://www.linkedin.com/in/mohamed-essadik-el-maghraoui-a62607361?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    description: "Not just photos, but fragments of life !",
+  },
+  {
+    name: "Sabrina El Khallouki",
+    Image: "/images/Igniters/Sabrina.jpg",
+    post: "Conseillère",
+    linkedin: "https://www.linkedin.com/in/sabrina-el-khallouki-312995257/",
+    description:
+      "إذا امتلأت كفّ اللئيم من الغنى تمايل إعجاباً وقال أنا أنا — ولكن كريم الأصل كالغصن كلّما تحمّل أثماراً تواضع وانحنى !  ",
+  },
+  {
+    name: "Wassima El Kidach",
+    Image: "/images/Igniters/Wassima.jpeg",
+    post: "Conseillère",
+    linkedin:
+      "https://www.linkedin.com/in/wassima-el-kidach-419976335?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    description: "Great journeys begin with small steps 🌍✨!",
+  },
+  {
+    name: "Mustapha El Assaoui",
+    Image: "/images/Igniters/Mustapha.jpg",
+    post: "Conseiller",
+    linkedin: "https://www.linkedin.com/in/el-assaoui-mustapha/",
+    description: "Discipline is my way of being lazy later without guilt !",
+  },
+  {
+    name: "Abderrahmane Kayouh",
+    Image: "/images/Igniters/Abderehmane.jpeg",
+    post: "Conseiller",
+    linkedin: "https://www.linkedin.com/in/abderrahman-kayouh-5883192b5/",
+    description:
+      "✨Passion is the engine that turns challenges into solutions and life into meaningful design✨!",
   },
 ];
 
